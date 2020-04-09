1e1243b1ba17261479f760e9066ca0171d1a556e
web-coder-maksim <clankedmax@gmail.com>
2020-04-09 22:56:38 +0300

    fixed products added slider section and added release section

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,24 @@
 $(function(){
 
+    $(".link-rating").rateYo({
+        rating: 4.5,
+        starWidth: "16px",
+        readOnly: true
+    });
+
+    $(".star-rating").rateYo({
+        rating: 4.5,
+        starWidth: "16px",
+        readOnly: true
+    });
     
+
+    $('.slider__inner').slick({
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.png" alt=""></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.png" alt=""></img>',
+        fade: true
+    });
+
+    var mixer = mixitup('.common-inner');
 
 });
