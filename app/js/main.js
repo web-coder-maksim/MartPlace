259f81088bd213d565c0462f8ed6f9e969025376
web-coder-maksim <clankedmax@gmail.com>
2020-04-12 14:45:07 +0300

    added items section and choose section

--- app/js/main.js
+++ app/js/main.js
@@ -15,8 +15,15 @@ $(function(){
 
     $('.slider__inner').slick({
         prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.png" alt=""></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.png" alt=""></img>'
+        // fade: true
+    });
+
+    $('.followers-slider').slick({
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.png" alt=""></img>',
         nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.png" alt=""></img>',
-        fade: true
+        slidesToShow: 3,
+        slidesToScroll: 3
     });
 
     var mixer = mixitup('.common-inner');
